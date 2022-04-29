a4de9b0b0bfe7afdc81d5aaf53f037d4e1014fdb
timdean27 <timdean27@yahoo.com>
2022-04-29 13:26:28 -0400

    changes

--- Db/connection.js
+++ Db/connection.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const mongoURI =
   process.env.NODE_ENV === 'production'
     ? process.env.DB_URL
-    // : 'mongodb://localhost/sneaker';
-    :process.env.DEV_DB_URL
+    : 'mongodb://localhost/sneaker';
+    //:process.env.DEV_DB_URL
 
 mongoose.connect(mongoURI, {
   })
