c567ce3998112c2e07b2946a96a0004caa8d97f3
timdean27 <timdean27@yahoo.com>
2022-04-29 13:52:36 -0400

    test

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.use('/accounting', requireAccountingRouter);
 app.set('port', process.env.PORT || 3000);
 
 
-app.listen(3000, () => {
-    console.log("app listening on port 3000")
-  })
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
