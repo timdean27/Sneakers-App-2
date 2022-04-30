fc0036985739fd019f09ce960406210e23408c99
timdean27 <timdean27@yahoo.com>
2022-04-30 14:28:40 -0400

    chnaged local host to 4000

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use('/releases', requireReleaseRouter);
 app.use('/accounting', requireAccountingRouter);
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("Express server listening on port", this.address().port, app.settings.env);
 });
